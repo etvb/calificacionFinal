e29f7fad3729fa8caa56ad2316bbfda349b956c1
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-08-30 05:52:13 -0500

    Agregamos las variables, solicitamos los datos y calculamos

--- PATS-16-5-calificacionFinal/src/pats/pkg16/pkg5/calificacionfinal/PATS165CalificacionFinal.java
+++ PATS-16-5-calificacionFinal/src/pats/pkg16/pkg5/calificacionfinal/PATS165CalificacionFinal.java
@@ -5,6 +5,8 @@
  */
 package pats.pkg16.pkg5.calificacionfinal;
 
+import java.util.Scanner;
+
 /**
  *
  * @author edgardo
@@ -16,6 +18,22 @@ public class PATS165CalificacionFinal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        float participacion, examen1, examen2, examenFinal,calFinal;
+        Scanner teclado = new Scanner(System.in);
+        
+        //Ingresar datos y calculamos
+        System.out.print("Participacion: ");
+        participacion = (teclado.nextFloat())*.10f;
+        System.out.print("Primer Examen: ");
+        examen1 = (teclado.nextFloat())*.25f;
+        System.out.print("Segundo Examen: ");
+        examen2 = (teclado.nextFloat())*.25f;
+        System.out.print("Examen Final: ");
+        examenFinal = (teclado.nextFloat())*.40f;
+        
+        calFinal = participacion + examen1 + examen2 + examenFinal; 
+        System.out.println(calFinal);
+
     }
     
 }
